1c1a03ef5554be61960991f8c00c13cc0accdd4c
DanCvejn <daniel.cvejn@pslib.cz>
2020-11-09 10:57:06 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Mouse Move
+
+$('.background').mousemove(function(e){
+    const moveX = (e.pageX * -1/30);
+    const moveY = (e.pageY * -1/30);
+    $(this).css('background-position', moveX + 'px ' + moveY + 'px')
+})
+
